50a51a6fec4cd75afa83058fdf8edfbab71299fc
SamuelKrakheckeDias <161588543+SamuelKrakheckeDias@users.noreply.github.com>
2024-08-14 08:19:21 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let vetor = [];
+
+function adicionarValor() {
+
+  const valor = Number(document.getElementById('valor').value);
+
+
+
+  if (!isNaN(valor)) {
+    vetor.push(valor);
+
+
+
+    const tabela = document.getElementById('tabelaValores').getElementsByTagName('tbody')[0];
+
+
+    const novaLinha = tabela.insertRow();
+    const celula = novaLinha.insertCell(0);
+    celula.textContent = valor;
+
+    document.getElementById('valor').value = '';
+    document.getElementById('valor').focus();
+
+  }
+}
